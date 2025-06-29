8f7b99f18f4d6ba8e60e8cebdc2af21c7fadda41
cbravos5 <cnbravos5@gmail.com>
2025-06-29 20:24:39 -0300

    feat: add search

--- frontend/src/pages/Items/index.js
+++ frontend/src/pages/Items/index.js
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { useData } from "../../state/DataContext";
 import { Link } from "react-router-dom";
+import { useDebounce } from '@uidotdev/usehooks';
+import Skeleton from 'react-loading-skeleton';
 import styles from "./styles.module.css";
 
 function Items() {
   const [isLoading, setIsLoading] = useState(true);
   const { items, setItems, fetchItems } = useData();
 
+  const [search, setSearch] = useState('');
+  const debouncedSearch = useDebounce(search, 500); // debounced value to prevent multiple fetch triggers
+
+  const onInputChange = (e) => {
+    setSearch(e.target.value);
+  }
+
   useEffect(() => {
     let active = true;
+    setIsLoading(true);
 
-    fetchItems()
+    fetchItems({ search: debouncedSearch })
       .then((response) => active && setItems(response))
       .catch(console.error)
       .finally(() => active && setIsLoading(false));
@@ -18,20 +28,29 @@ function Items() {
     return () => {
       active = false;
     };
-  }, [fetchItems]);
-
-  if (isLoading) return <p>Loading...</p>;
+  }, [fetchItems, debouncedSearch]);
 
   return (
     <>
       <div className={styles.searchContainer}>
-        <input className={styles.search} type="text" id="search" placeholder="Search items..." />
+        <input
+          className={styles.search}
+          aria-label="Search items"
+          type="text"
+          placeholder="Search items..."
+          onChange={onInputChange}
+        />
       </div>
 
       <div className={styles.itemsContainer}>
         <ul className={styles.itemsList}>
-          {items.map((item) => (
-            <li key={item.id} className={styles.listItem}>
+          {isLoading && Array.from({ length: 25 }).map((_, i) => (
+            <li key={i}>
+              <Skeleton height={52} />
+            </li>
+          ))}
+          {!isLoading && items.map((item) => (
+            <li key={item.id} className={`${styles.listItem} ${styles.itemAppear}`}>
               <Link to={"/items/" + item.id} className={styles.listItemLink}>
                 {item.name}
               </Link>
--- frontend/src/state/DataContext.js
+++ frontend/src/state/DataContext.js
@@ -5,8 +5,10 @@ const DataContext = createContext();
 export function DataProvider({ children }) {
   const [items, setItems] = useState([]);
 
-  const fetchItems = useCallback(async () => {
-    const res = await fetch('http://localhost:3001/api/items?limit=500');
+  const fetchItems = useCallback(async ({ search }) => {
+    const params = new URLSearchParams({ q: search, limit: 500 });
+
+    const res = await fetch('http://localhost:3001/api/items?' + params.toString());
     const json = await res.json();
     return json;
   }, []);
